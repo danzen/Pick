c05ae7ac9e0e59ca440b2e3e4493aedeb1c66774
Dan Zen <door@danzen.com>
2019-01-23 18:05:47 -0500

    pick.js added
    Added the Pick() class in a JS file

---
+++ src/pick.js
@@ -0,0 +1,135 @@
+/*
+Pick class - Dan Zen for https://zimjs.com
+A system to pass in options as a parameter
+and then pick the option inside the function
+
+We might have a particle emitter with variable shapes.
+If we pick the shape outside the function
+then it would emmit the chosen shape over and over
+Instead, we can pass in a Pick object
+and the Emitter can choose inside to emmit different particles
+
+Pick lets you pass a range, an array of choices, a series
+or any function that returns a value as options
+
+pass in a new Pick(options) as a parameter
+and choose from options inside function
+using Pick.choose(parameter)
+
+// EXAMPLE
+function interval(time, call) {
+    function makeCall(t)
+        setTimeout(function(){
+            call();
+            makeCall(Pick.choose(time));
+        }, t);
+    }
+}
+// every second - Pick.choose() passes numbers, strings, through
+interval(1000, function () {console.log("calling");});
+
+// one, two or three seconds randomly
+var pick = new Pick([1000, 2000, 3000]);
+interval(pick, function () {console.log("calling");});
+
+// one, two or three seconds randomly using a Pick literal
+interval([1000, 2000, 3000], function () {console.log("calling");});
+
+// a range between one and three seconds
+interval({min:1000, max:3000}, function () {console.log("calling");});
+
+// a range between one and three seconds - repeating after 3 choices
+var pick = new Pick({min:1000, max:3000}).num(3);
+interval(pick, function () {console.log("calling");});
+
+// one, two and three seconds consecutively then repeat
+interval(Pick.series(1000,2000,3000), function () {console.log("calling");});
+
+// pick based on a function result
+// here we return a Pick literal - Pick is recursive
+// so if daytime, the interval runs randomly more slowly
+function dayNight() {
+    if (new Date.getHours() > 11) return [100,200,300];
+    else return [1000, 2000, 3000];
+}
+interval(dayNight, function () {console.log("calling");});
+
+EXAMPLE
+Loop through a Pick
+var pick = new Pick({max:10, integer:true});
+var status = pick.loop(20, function(amount, i, total) {
+    if (amount == 5) return; // like a continue in a for loop
+    if (amount == 7) return "lose" // like a break in a for loop
+    if (amount < 2) console.log("low")
+    else if (amount > 8) console.log("high")
+    console.log("completed " + i + " of " + total);
+})
+console.log(status=="lose"?"lose":"win");
+*/
+
+function Pick(choices)  {
+    this.choices = choices;
+    this.num = function(num) {
+        var s = [];
+        for (var i=0; i<num; i++) {s.push(Pick.choose(this))}
+        this.choices = Pick.series(s);
+        return this;
+    }
+    var that = this;
+    this.loop = function(num, call) {
+        var r;
+        for (var i=0; i<num; i++) {
+            r = call(Pick.choose(that), i, num);
+            if (typeof r != 'undefined') return r;
+        }
+     }
+};
+Pick.prototype.type = "Pick";
+Pick.series = function() {
+    var array;
+    if (arguments.length == 0) return function(){};
+    if (arguments.length == 1 && Array.isArray(arguments[0])) array = arguments[0];
+    else array = arguments;
+    var count = 0;
+    var f = function() {
+        return array[(count++)%array.length];
+    }
+    f.array = array;
+    return f;
+}
+Pick.rand = function(a, b, integer, negative) {
+    if (a==null && b==null) return Math.random();
+    if (a==null || isNaN(a)) a = 0;
+    if (b==null || isNaN(b)) b = 0;
+    if (a%1!=0 || b%1!=0) integer = false;
+    if (integer==null) integer = true;
+    if (negative) if (Math.random()>.5) {a*=-1; b*=-1;};
+    if (integer) if (a>b) {a++;} else if (b>a) {b++;}
+    var r;
+    if (a == 0 && b == 0) return 0;
+    else if (b == 0) r = Math.random()*a;
+    else r = Math.min(a,b)+Math.random()*(Math.max(a,b)-Math.min(a,b));
+    if (integer) return Math.floor(r);
+    else return r;
+}
+Pick.choose = function(obj, literal) {
+    if (literal == null) literal = true;
+    if (obj.type=="Pick" || literal) {
+        var c = obj.choices || obj;
+        if (Array.isArray(c)) {
+            var val = c[Math.floor(Math.random()*(c.length))];
+            return Pick.choose(val); // recursive
+        } else if (c.constructor === {}.constructor) {
+            if (!zot(c.noPick)) return c.noPick; // a passthrough for arrays and functions
+            if (zot(c.max)) return c;
+            if (zot(c.integer)) c.integer = false;
+            var val = Pick.rand(c.min, c.max, c.integer, c.negative);
+            return val;
+        } else if (typeof c == "function") {
+            return Pick.choose((c)()); // recursive
+        }
+        return obj;
+    } else {
+        return obj;
+    }
+}
